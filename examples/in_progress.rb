1408429fc4caf6521411d1f7ce7c9a34cadd26e9
Michael Lee <lee@soi2.org>
2008-05-29 09:45:13 -0500

    Adding examples.

---
+++ examples/in_progress.rb
@@ -0,0 +1,26 @@
+puts "Hello!"
+puts "I'm your instructor, Michael.  Welcome to CS 105!"
+
+puts 2 + 5
+puts 10 - 3
+puts 2 * 6 - 3 * 3
+puts 2 * (6 - 3) * 3
+puts 3 / 6
+puts 3.0 / 6.0
+
+puts "What's" + " " + "this?"
+puts "Hi!" * 5
+
+puts "=" * 10
+puts "Woohoo!"
+puts "=" * 10
+
+x = 10
+y = 20
+puts x * y
+
+s = "Yippeeeeee!!!!"
+l = "Yippeeeeee!!!!".length
+puts "*" * l
+puts s
+puts "*" * l
