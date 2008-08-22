c1192e84bac530f486fe1f4f203571ce93db9ea4
Michael Lee <lee@soi2.org>
2008-08-21 22:00:21 -0500

    Prettifying lab 1 output -- correct number of stars.

--- lab1/lab1support.rb
+++ lab1/lab1support.rb
@@ -1,6 +1,6 @@
 def welcome(name)
-  puts "*" * 80
+  puts "*" * 51
   puts "Welcome to CS 105, #{name}."
   puts "Congratulations on writing your first Ruby program!"
-  puts "*" * 80
+  puts "*" * 51
 end
