ef46e817200f48eed65fd860b619c979b9c7a194
Michael Lee <lee@soi2.org>
2009-09-03 08:00:08 -0500

    Removing old lab files

--- lab01/lab1support.rb
+++
@@ -1,6 +0,0 @@
-def welcome(name)
-  puts "*" * 51
-  puts "Welcome to CS 105, #{name}."
-  puts "Congratulations on writing your first Ruby program!"
-  puts "*" * 51
-end
--- lab02/lab2ex3.rb
+++
@@ -1,5 +0,0 @@
-puts Hello!
-puts 'Good job with your first lab"
-puts 'This is the second of'
-puts 15 - '2'
-puts 'labs.'
